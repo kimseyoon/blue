d766f6c0b022ac5b4a969a073f147ae8adf25b09
seyoon <seyoon@seyoonui-MacBook-Pro.local>
2017-03-23 21:38:33 +0900

    [Major]TotalView 추가 및 기능추가
    전체기사보기 뷰 추가
    CSS전면 재수정
    구독하기 기능 추가
    전체기사보기 뷰일 경우 이전 다음버튼 누를 경우 다른 기사 보여주기#

--- day4/main.js
+++ day4/main.js
@@ -8,18 +8,39 @@ ns.view.headerObj = {
 
   registerEvent : function(){
     this.$header.addEventListener("click", function(e){
+      e.preventDefault();
       var target = e.target;
+
       if(target.closest(".btnMove")){
-        this.movePrevAndNextContent(target);
+        this.movePrevAndNext(target);
+        return;
       }
+
+      if(target.closest(".location li")){
+        this.moveTab(target);
+        return;
+      }
+
     }.bind(this))
   },
 
-  movePrevAndNextContent : function(target){
-    if(target.closest(".left")){
-      ns.dispatcher.emit({"type" : "movePrevAndNextContent"}, ["prev"]);
+  moveTab : function(target){
+    var allLi = ns.util.$(".mainArea").children;
+    var tabContId = target.getAttribute("href");
+    for(var i = 0; i < allLi.length; i = i + 1){
+      allLi[i].classList.remove("show");
+    }
+    ns.util.$(tabContId).classList.add("show");
+
+    var statusView = target.closest("li").getAttribute("id");
+    ns.dispatcher.emit({"type" : "changeStatusView"}, [statusView]);
+  },
+
+  movePrevAndNext : function(target){
+    if(target.classList.contains("left")){
+      ns.dispatcher.emit({"type" : "movePrevAndNext"}, ["prev"]);
     }else{
-      ns.dispatcher.emit({"type" : "movePrevAndNextContent"}, ["next"]);
+      ns.dispatcher.emit({"type" : "movePrevAndNext"}, ["next"]);
     }
   }
 
@@ -73,10 +94,10 @@ ns.view.lnbObj = {
         result = "";
     for(var i = 0; i < data.length; i = i + 1){
       if(i === 0){
-        result += "<li class='select'>"+data[i]+"</li>";
+        result += "<li class='select' data-id='"+data[i].id+"'>"+data[i].title+"</li>";
         continue;
       }
-      result += "<li>"+data[i]+"</li>";
+      result += "<li data-id='"+data[i].id+"'>"+data[i].title+"</li>";
     }
     ulHtml = ulHtml.replace("{newsTitleList}", result);
     this.$lnb.innerHTML = ulHtml;
@@ -125,11 +146,62 @@ ns.view.Content = function(){
   return {
     $content : ns.util.$(".mainArea .content")
   }
-}
+};
+
+ns.view.totalPressObj = {
+  init : function(){
+    this.registerEvent();
+  },
+
+  registerEvent : function(){
+    this.$totalPressArea.addEventListener("click", function(e){
+      var target = e.target;
+
+      if(target.closest(".btnSbscription")){
+        this.changeSubscription(target);
+      }
+    }.bind(this));
+  },
+
+  changeSubscription : function(target){
+    var dataId = target.getAttribute("data-id");
+    if(target.classList.contains("yes")){
+      ns.dispatcher.emit({"type" : "changeSubscription"}, [dataId, "yes"]);
+    }else{
+      ns.dispatcher.emit({"type" : "changeSubscription"}, [dataId, "no"]);
+    }
+  },
+
+  renderView : function(data){
+    var pressListHtml =  ns.util.$("#pressListTemplate").innerHTML,
+        result = "",
+        str = "";
+    for(var i = 0; i < data.length; i++){
+      result += pressListHtml.replace("{title}", data[i].title);
+      if(data[i].subscription === "yes"){
+        str = "<a href='#' class='no' data-id='"+data[i].id+"'>해지</a>"
+      }else{
+        str = "<a href='#' class='yes' data-id='"+data[i].id+"'>구독</a>"
+      }
+      result = result.replace("{btnList}", str);
+    }
+    var pressList = ns.util.$(".totalPressArea .pressList");
+    pressList.innerHTML = result;
+  }
+};
+
+ns.view.TotalPress = function(){
+  return {
+    $totalPressArea : ns.util.$(".totalPressArea")
+  }
+};
 
 ns.modelObj = {
-  setListData : function(data){
-    this.newsList = data;
+  setSubscriptionListData : function(data){
+    function _checkSubscription(item){
+      return item.subscription === "yes";
+    }
+    this.subscriptionList = data.filter(_checkSubscription);
   },
 
   setCurrentContentIndex : function(index){
@@ -141,24 +213,101 @@ ns.modelObj = {
   },
 
   getAllDataLength : function(){
-    return this.newsList.length;
+    return this.subscriptionList.length;
   },
 
   getAllData : function(){
-    return this.newsList;
+    return this.subscriptionList;
   },
 
   getTargetData : function(index){
-    return this.newsList[index];
+    return this.subscriptionList[index];
   },
 
   getAllTitleList : function(){
     var titleArr = [];
-    this.newsList.forEach(function(item){
+    this.subscriptionList.forEach(function(item){
       titleArr.push(item.title);
     })
     return titleArr;
   },
+/*
+  removeData : function(index){
+    this.subscriptionList.splice(index, 1);
+  },
+*/
+  setTotalPressListData : function(data){
+    this.totalPresslList = data;
+  },
+
+  getTotalPressListData : function(data){
+    return this.totalPresslList;
+  },
+
+  setSubscription : function(dataId, subscription){
+    var dataId = parseInt(dataId);
+    this.totalPresslList.forEach(function(item){
+      if(item.id === dataId){
+        item.subscription = subscription;
+      }
+    })
+    this.setSubscriptionListData(this.totalPresslList);
+  },
+
+  getIdOfCurrentContent : function(){
+    var titleList = ns.util.$(".myNewsArea nav ul").children;
+    return titleList[this.currentContentIndex].getAttribute("data-id");
+  },
+
+  setStatusView : function(status){
+    this.statusView = status;
+  },
+
+  getStatusView : function(){
+    return this.statusView;
+  },
+
+
+
+  setTotalViewPage : function(){
+    var LISTNUM = 12;
+    var totalPressList = this.totalPresslList;
+    this.totalViewPage = Math.floor(totalPressList.length / LISTNUM) + 1;
+  },
+
+  getTotalViewPage : function(){
+    return this.totalViewPage;
+  },
+
+
+  setTotalViewPageArr : function(){
+    var LISTNUM = 12;
+    var totalPressList = this.totalPresslList;
+    this.setTotalViewPage();
+  //  var page = Math.floor(totalPressList.length / LISTNUM) + 1;
+    var num = 0;
+    //debugger;
+    for(var i = 0; i < this.totalViewPage; i++){
+      this.totalViewPageArr[i] = totalPressList.slice(num * LISTNUM, (num + 1) * LISTNUM);
+      num++;
+    }
+  },
+
+  getTotalViewPageArr : function(){
+    return this.totalViewPageArr;
+  },
+
+  getTotalViewPageArrPiece : function(index){
+    return this.totalViewPageArr[index];
+  },
+
+  getTotalViewCurrentPage : function(){
+    return this.totalViewCurrentPage;
+  },
+
+  setTotalViewCurrentPage : function(index){
+    this.totalViewCurrentPage = index
+  }
 
 /*
   getInitViewData : function(index){
@@ -167,15 +316,17 @@ ns.modelObj = {
     ns.dispatcher.emit({"type" : "lnbContentViewRender"}, [titleArr, targetData]);
   },
 */
-  removeData : function(index){
-    this.newsList.splice(index, 1);
-  }
 };
 
 ns.Model = function(){
   return{
-    newsList : [],
-    currentContentIndex : 0
+    totalPresslList : [],
+    subscriptionList : [], // 구독리스트
+    currentContentIndex : 0,
+    statusView : "",
+    totalViewPageArr : [],
+    totalViewPage : 0,
+    totalViewCurrentPage : 0
   }
 }
 
@@ -186,14 +337,23 @@ ns.controllerObj = {
 
   chain : function(){
     ns.dispatcher.register({
-      "lnbContentViewRender" : function(titleArr, allData){
+      "lnbContentViewRender" : function(subscriptionAllData, targetAllData){
         this.lnbView.showCurrentPage(this.model.getCurrentContentIndex());
         this.lnbView.showTotalPage(this.model.getAllDataLength());
-        this.lnbView.renderView(titleArr);
-        this.contentView.renderView(allData);
+        this.lnbView.renderView(subscriptionAllData);
+        this.contentView.renderView(targetAllData);
       }.bind(this),
 
-      "movePrevAndNextContent" : function(direction){
+      "movePrevAndNext" : function(direction){
+        var statusView = this.model.getStatusView();
+        if(statusView === "myNews"){
+          ns.dispatcher.emit({"type" : "moveContent"}, [direction]);
+        }else{
+          ns.dispatcher.emit({"type" : "moveTotalView"}, [direction]);
+        }
+      }.bind(this),
+
+      "moveContent" : function(direction){
         var contentIndex = this.model.getCurrentContentIndex();
         var titleDataLength = this.model.getAllTitleList().length;
         var targetData = "";
@@ -220,12 +380,46 @@ ns.controllerObj = {
         this.contentView.renderView(targetData);
       }.bind(this),
 
+      "moveTotalView" : function(direction){
+        //ns.dispatcher.emit({"type" : "totalPressViewRender"}, [this.model.getTotalViewPageArrPiece(0)]);
+
+        var totalPage = this.model.getTotalViewPage();
+        var currentPage = this.model.getTotalViewCurrentPage();
+
+        if(direction === "prev"){
+          currentPage--;
+          if(currentPage === -1){
+            currentPage = totalPage - 1;
+          }
+        }else{
+          currentPage++;
+          if(currentPage >= totalPage){
+            currentPage = 0;
+          }
+        }
+        //console.log(currentPage);
+
+        ns.dispatcher.emit({"type" : "totalPressViewRender"}, [this.model.getTotalViewPageArrPiece(currentPage)]);
+        this.model.setTotalViewCurrentPage(currentPage);
+
+        //ns.dispatcher.emit({"type" : "totalPressViewRender"}, [this.model.getTotalViewPageArrPiece(index)]);
+        //this.model.setTotalViewCurrentPage(index);
+
+      }.bind(this),
+
       "cancelSubscription" : function(){
         var contentIndex = this.model.getCurrentContentIndex();
-        this.model.removeData(contentIndex);
+        //this.model.removeData(contentIndex);
+        var currentContentId= this.model.getIdOfCurrentContent();
+        this.model.setSubscription(currentContentId, "no");
         this.model.setCurrentContentIndex(0);
+
+        this.model.setSubscriptionListData(this.model.getTotalPressListData());
+
         //this.model.getInitViewData(0);
-        ns.dispatcher.emit({"type" : "lnbContentViewRender"}, [this.model.getAllTitleList(), this.model.getTargetData(0)]);
+        //ns.dispatcher.emit({"type" : "totalPressViewRender"}, [this.model.getTotalPressListData()]);
+        ns.dispatcher.emit({"type" : "totalPressViewRender"}, [this.model.getTotalViewPageArrPiece(0)]);
+        ns.dispatcher.emit({"type" : "lnbContentViewRender"}, [this.model.getAllData(), this.model.getTargetData(0)]);
       }.bind(this),
 
       "moveClickedList" : function(index){
@@ -236,12 +430,31 @@ ns.controllerObj = {
         this.contentView.renderView(targetData);
       }.bind(this),
 
+      "totalPressViewRender" : function(data){
+        this.totalPressView.renderView(data);
+      }.bind(this),
+
+      "changeSubscription" : function(dataId, subscription){
+        this.model.setSubscription(dataId, subscription);
+        //ns.dispatcher.emit({"type" : "totalPressViewRender"}, [this.model.getTotalPressListData()]);
+        ns.dispatcher.emit({"type" : "totalPressViewRender"}, [this.model.getTotalViewPageArrPiece(0)]);
+        ns.dispatcher.emit({"type" : "lnbContentViewRender"}, [this.model.getAllData(), this.model.getTargetData(0)]);
+      }.bind(this),
+
+      "changeStatusView" : function(status){
+        this.model.setStatusView(status);
+      }.bind(this),
+
       "initView" : function(){
         this.headerView.init();
         this.lnbView.init();
         this.contentView.init();
+        this.totalPressView.init();
+
+        //ns.dispatcher.emit({"type" : "totalPressViewRender"}, [this.model.getTotalPressListData()]);
+        ns.dispatcher.emit({"type" : "totalPressViewRender"}, [this.model.getTotalViewPageArrPiece(0)]);
         //this.model.getInitViewData(this.model.getCurrentContentIndex());
-        ns.dispatcher.emit({"type" : "lnbContentViewRender"}, [this.model.getAllTitleList(), this.model.getTargetData(0)]);
+        ns.dispatcher.emit({"type" : "lnbContentViewRender"}, [this.model.getAllData(), this.model.getTargetData(0)]);
       }.bind(this)
     })
   }
@@ -252,6 +465,7 @@ ns.Controller = function(obj){
     headerView : obj.headerView,
     lnbView : obj.lnbView,
     contentView : obj.contentView,
+    totalPressView :obj.totalPressView,
     model : obj.model
   }
 }
@@ -298,15 +512,27 @@ document.addEventListener("DOMContentLoaded", function(){
   var contentView = ns.view.Content();
   Object.setPrototypeOf(contentView, ns.view.contentObj);
 
+  var totalPressView = ns.view.TotalPress();
+  Object.setPrototypeOf(totalPressView, ns.view.totalPressObj);
+
   var control = ns.Controller({
-    headerView : headerView, lnbView : lnbView, contentView : contentView, model : model
+    headerView : headerView, lnbView : lnbView, contentView : contentView, totalPressView : totalPressView, model : model
   });
   Object.setPrototypeOf(control, ns.controllerObj);
 
   control.chain();
+
   ns.util.runAjax(function(e){
     var data = JSON.parse(e.target.responseText);
-    model.setListData(data);
+
+    model.setTotalPressListData(data);
+    model.setSubscriptionListData(data);
+
+    model.setStatusView("total");
+
+
+    model.setTotalViewPageArr();
+
     control.init();
   }, "GET", "newslist.json")
 });
